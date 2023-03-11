b9c94170f62303a90347c8dd07de6e5c66a6b759
Babajide Tomoshegbo <babajide234@gmail.com>
2023-03-11 17:53:12 +0100

    registration done

--- src/components/Forms/RegisterForm.jsx
+++ src/components/Forms/RegisterForm.jsx
@@ -1,18 +1,57 @@
 import React from 'react'
 import Buttons from '../buttons/Buttons'
 import { AuthInputs } from '../Inputs'
+import { Formik, ErrorMessage  } from 'formik'
+import useUserStore from '../../store/userSlice'
 
 const RegisterForm = () => {
+  const Loading = useUserStore(state=> state.loading);
+  const register = useUserStore(state=> state.register);
+
+  const initialValues = {
+    username: "",
+    email: "",
+    phone: "",
+    passcode: "",
+    referral_code: ""
+  };
+
+  const onSubmit = (values, { setSubmitting }) => {
+
+    const data = {
+      username: values.username,
+      email: values.email,
+      passcode: values.passcode,
+      phone: values.phone,
+      referral_code: values.referral_code
+    }
+
+    register(data)
+    setSubmitting(false);
+
+  };
   return (
     <div className=" flex flex-col h-full justify-between">
-        <div className="">
-            <AuthInputs placeholder={'Username'} type={'text'}/>
-            <AuthInputs placeholder={'Email'} type={'text'}/>
-            <AuthInputs placeholder={'Phone'} type={'text'}/>
-            <AuthInputs placeholder={'Password'} type={'text'}/>
-            <AuthInputs placeholder={'Refferal Code'} type={'text'}/>
-        </div>
-        <Buttons type={'primary'} to={''}>Register</Buttons>
+        <Formik initialValues={initialValues}  onSubmit={onSubmit}>
+        {({ isSubmitting, values, handleSubmit,handleChange }) => (
+          <form action="" onSubmit={handleSubmit}>
+              <div className="">
+                  <AuthInputs name='username' onChange={handleChange} placeholder={'Username'} type={'text'}/>
+                  <AuthInputs name='email' onChange={handleChange} placeholder={'Email'} type={'email'}/>
+                  <AuthInputs name='phone' onChange={handleChange} placeholder={'Phone'} type={'text'}/>
+                  <AuthInputs name='passcode' onChange={handleChange} placeholder={'Password'} type={'password'}/>
+                  <AuthInputs name='referral_code' onChange={handleChange} placeholder={'Refferal Code'} type={'text'}/>
+              </div>
+              <button 
+                type="submit"
+                className="w-full py-4 flex justify-center items-center text-lg font-bold rounded-full bg-primary text-default"
+              >
+                { Loading ? <Spinner/> : 'Register'}
+                {/* <Spinner/> */}
+              </button>
+          </form>
+        )}
+        </Formik>
     </div>
   )
 }
